bbcfa191b131b71e3c75cb5a0dd7e70ace4d1833
Haur Kang <haur.kang@gmail.com>
2018-08-24 16:18:33 +0100

    installed angular2 flash messenges

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDXS-5H92zUwJAat53Qz3BgRUqd_cmUtR4',
+    authDomain: 'balancesheet-88f60.firebaseapp.com',
+    databaseURL: 'https://balancesheet-88f60.firebaseio.com',
+    projectId: 'balancesheet-88f60',
+    storageBucket: 'balancesheet-88f60.appspot.com',
+    messagingSenderId: '791364340714'
+  }
 };
